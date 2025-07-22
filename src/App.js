8b44e88370c49a174d0bb9f5746918555e8762b3
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2025-07-22 09:16:50 -0600

    Adding css style to the calculator

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [startingGravity, setStartingGravity] = useState(0);
-  const [finalGravity, setFinalGravity] = useState(0);
+  const [startingGravity, setStartingGravity] = useState(1.05);
+  const [finalGravity, setFinalGravity] = useState(1.01);
   const [abv, setAbv] = useState(0);
 
   useEffect(() => {
@@ -17,21 +17,49 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>Brewing APV Calculator</h1>
+        <h1>ABV Calculator</h1>
       </header>
-      <section className="flex flex-col">
-        <label>
-          Starting Gravity
-          <input type="number" value={startingGravity} onChange={(e) => setStartingGravity(e.target.value)} />
-        </label>
-        <label>
-          Final Gravity
-          <input type="number" value={finalGravity} onChange={(e) => setFinalGravity(e.target.value)} />
-        </label>
-        <div>
-          ABV: {abv}%
+
+      <section className="form">
+        <div className="form-group">
+          <label
+            className="form-label"
+            htmlFor="starting-gravity"
+          >
+            Original Gravity (OG)
+          </label>
+          <input 
+            type="number" 
+            className="form-input"
+            id="starting-gravity"
+            value={startingGravity} 
+            onChange={(e) => setStartingGravity(parseFloat(e.target.value) || 1.00)}
+          />
+        </div>
+        
+        <div className="form-group">
+          <label
+            className="form-label"
+            htmlFor="final-gravity"
+          >
+            Final Gravity (FG)
+          </label>
+          <input 
+            type="number" 
+            className="form-input"
+            id="final-gravity"
+            value={finalGravity} 
+            onChange={(e) => setFinalGravity(parseFloat(e.target.value) || 1.00)}
+          />
         </div>
       </section>
+
+      <div className="calculator-display">
+        <div className="result-label">Alcohol By Volume</div>
+        <div className="result-value">
+          {abv}%
+        </div>
+      </div>
     </div>
   );
 }
