42c581936e5cea49765e52e5f722623f1c0d0aec
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2025-07-24 15:07:44 -0600

    Fixing bug with svg

--- src/app/components/ai/names.tsx
+++ src/app/components/ai/names.tsx
@@ -31,7 +31,7 @@ export default function Names({
               cy='12'
               r='10'
               stroke='currentColor'
-              stroke-width='4'
+              strokeWidth='4'
             ></circle>
             <path
               className='opacity-75'
